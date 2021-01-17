92686377149a4da6fd0c177d9fcabbdab62d4181
Nathan Andrews <nandrews.programming@gmail.com>
2021-01-17 13:30:48 -0500

    Added game logic
    Added functionality where once a square is clicked, the response <p> tag will display whether or not the user picked correctly

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let answer = '';
+
 document.getElementById('new-colors-button').addEventListener('click', event => {
+    document.getElementById('response').innerHTML = '';
     const squares = document.getElementById('squares-container').children;
     for(let i = 0; i<squares.length; i++){
         let currentSquare = squares[i];
         currentSquare.style.backgroundColor = generateColorString();
     }
-    let answer = squares[Math.floor(Math.random()*(squares.length-1))].style.backgroundColor;
+    answer = squares[Math.floor(Math.random()*(squares.length-1))].style.backgroundColor;
     document.getElementById('answer-color').innerHTML = answer;
+});
+
+document.getElementById('squares-container').addEventListener('click', event => {
+    if(event.target.id === 'squares-container') { return; }
+    const square = event.target;
+    const response = document.getElementById('response');
+    if(square.style.backgroundColor === answer){
+        response.innerHTML = 'CORRECT';
+    }
+    else{
+        response.innerHTML = 'INCORRECT'
+    }
 });
 
 function generateColorString(){
